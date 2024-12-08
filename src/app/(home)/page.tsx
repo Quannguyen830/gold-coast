148ce9a4a44615efb5a8367b203051da8926ba08
Quang (Austin) Dinh <thangdhm75@gmail.com>
2024-12-08 14:45:08 +1100

    bacd

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { supabase } from "../utils/supabase";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <button
+        onClick={() => {
+          supabase.auth.signInWithOAuth({
+            provider: "google",
+          });
+        }}
+      >
+        gg
+      </button>
     </>
   )
 }
