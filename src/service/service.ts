417abc6c12bc30f142999ddf94005a73f87393a2
quan <nguyenvietquan512@gmail.com>
2024-12-10 13:26:17 +1100

    Update service.ts

--- src/service/service.ts
+++ src/service/service.ts
@@ -14,14 +14,14 @@ export class GooglePhotosService {
   }
 
   private async getAccessToken() {
-    const responseFromAuthorize = await fetch("https://accounts.google.com/o/oauth2/v2/auth?client_id=1015137600762-411paej3c3d83dtte6n41ruoubvl34hh.apps.googleusercontent.com&redirect_uri=http://localhost:3000&response_type=code&scope=https://www.googleapis.com/auth/photoslibrary.readonly");
+    const responseFromAuthorize = await fetch("https://accounts.google.com/o/oauth2/v2/auth?client_id=1015137600762-411paej3c3d83dtte6n41ruoubvl34hh.apps.googleusercontent.com&redirect_uri=https://gold-coast-sandy.vercel.app&response_type=code&scope=https://www.googleapis.com/auth/photoslibrary.readonly");
     const responseFromAuthorizeJson = await responseFromAuthorize.json();
     const responseFromToken = await fetch("https://oauth2.googleapis.com/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: `code=${responseFromAuthorizeJson.code}&client_id=1015137600762-411paej3c3d83dtte6n41ruoubvl34hh.apps.googleusercontent.com&client_secret=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vOCCgZKfedjjpmBEpCgbLMyvNSec9tzb2oGa0ER7YPt1huvD3gXbo_uxRmXSG5qJrHy3jhWCdLqI4SKuoqjfxTIROE96Ua7fBeZJx5Ueeq4trG1G4SS7UVwepEqhij0n5LVzZGXf8aCgYKAd8SARMSFQHGX2MiFzAoM_Qbj3g8PYa4ncXcPg0170&redirect_uri=http://localhost:3000&grant_type=authorization_code`,
+      body: `code=${responseFromAuthorizeJson.code}&client_id=1015137600762-411paej3c3d83dtte6n41ruoubvl34hh.apps.googleusercontent.com&client_secret=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vOCCgZKfedjjpmBEpCgbLMyvNSec9tzb2oGa0ER7YPt1huvD3gXbo_uxRmXSG5qJrHy3jhWCdLqI4SKuoqjfxTIROE96Ua7fBeZJx5Ueeq4trG1G4SS7UVwepEqhij0n5LVzZGXf8aCgYKAd8SARMSFQHGX2MiFzAoM_Qbj3g8PYa4ncXcPg0170&redirect_uri=https://gold-coast-sandy.vercel.app&grant_type=authorization_code`,
     });
     const responseFromTokenJson = await responseFromToken.json();
     return responseFromTokenJson.access_token;
