5598790130ea0649ab04ee29c41d22d2252f8b5b
vietquanRMIT <nguyenvietquan512@gmail.com>
2024-12-16 10:48:15 +1100

    Fix secret-santa

--- src/components/common/share/ImageGrid2.tsx
+++ src/components/common/share/ImageGrid2.tsx
@@ -7,19 +7,19 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default async function ImageGrid2() {  
+export default async function ImageGrid2() {
   const result = await cloudinary.api.resources({
     resource_type: 'image',
     type: 'upload',
     prefix: 'gold-coast',
-    max_results: 50
+    max_results: 100
   });
 
   return (
     <div className="container mx-auto px-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div className={`col-start-1`}>
-          {result.resources.slice(0, 20).map((image: CloudinaryResource, index: number) => (              
+          {result.resources.slice(0, 20).map((image: CloudinaryResource, index: number) => (
             <img
               key={index}
               src={image.secure_url}
@@ -30,7 +30,7 @@ export default async function ImageGrid2() {
         </div>
 
         <div className={`col-start-2`}>
-          {result.resources.slice(21, 40).map((image: CloudinaryResource, index: number) => (              
+          {result.resources.slice(21, 40).map((image: CloudinaryResource, index: number) => (
             <img
               key={index}
               src={image.secure_url}
@@ -41,7 +41,7 @@ export default async function ImageGrid2() {
         </div>
 
         <div className={`col-start-3`}>
-          {result.resources.slice(41, 60).map((image: CloudinaryResource, index: number) => (              
+          {result.resources.slice(41, 60).map((image: CloudinaryResource, index: number) => (
             <img
               key={index}
               src={image.secure_url}
