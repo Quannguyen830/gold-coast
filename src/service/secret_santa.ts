0b2b80271c113c89616fb9fc99aac960457ee478
vietquanRMIT <nguyenvietquan512@gmail.com>
2024-12-17 19:48:44 +1100

    Update secret_santa.ts

--- src/service/secret_santa.ts
+++ src/service/secret_santa.ts
@@ -1,47 +1,59 @@
+import { sign } from "crypto";
+
 export const secretSantaDistribution = [
   {
     name: "Long",
     secret_santa: "Chi",
-    base_url: "profile/profile_chi.jpg"
+    base_url: "profile/profile_chi.jpg",
+    signIn: false,
   },
   {
     name: "Loc",
-    secret_santa: "Long",
-    base_url: "profile/profile_long.jpg"
+    secret_santa: "Quan",
+    base_url: "profile/profile_quan.JPG",
+    signIn: false,
   },
   {
     name: "Thao Anh",
     secret_santa: "Thang",
-    base_url: "profile/profile_thang.JPG"
+    base_url: "profile/profile_thang.JPG",
+    signIn: false
   },
   {
     name: "Chi",
-    secret_santa: "Jenny",
-    base_url: "profile/profile_jenny.jpg"
+    secret_santa: "Giang",
+    base_url: "profile/profile_giang.JPG",
+    signIn: false
   },
   {
     name: "Quyen",
-    secret_santa: "Quan",
-    base_url: "profile/profile_quan.JPG"
+    secret_santa: "Loc",
+    base_url: "profile/profile_loc.JPG",
+    signIn: false
   },
   {
     name: "Jenny",
     secret_santa: "Quyen",
-    base_url: "profile/profile_quyen.jpg"
+    base_url: "profile/profile_quyen.jpg",
+    signIn: false
   },
   {
     name: "Giang",
     secret_santa: "Thao Anh",
-    base_url: "profile/profile_anh.JPG"
+    base_url: "profile/profile_anh.JPG",
+    signIn: false
   },
   {
     name: "Thang",
-    secret_santa: "Giang",
-    base_url: "profile/profile_giang.JPG"
+    secret_santa: "Long",
+    base_url: "profile/profile_long.jpg",
+    signIn: false
   },
   {
     name: "Quan",
-    secret_santa: "Loc",
-    base_url: "profile/profile_loc.JPG"
+    secret_santa: "Jenny",
+    base_url: "profile/profile_jenny.jpg",
+    signIn: false
   },
 ]
+
